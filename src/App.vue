6c47d35d123d1e769754e84d887bf16558173168
AkakiyBerula <vlad5dyakun.gmail.com>
2024-02-22 20:04:12 +0200

    Темна та Світла Теми: Створіть переключатель тем, який динамічно змінює стилі всього додатку між темною та світлою темами.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,33 @@
 <template>
-  <div>
-    <HelloWorld msg="Hello!" />
-    <FormEvent />
-
-    <v-divider></v-divider>
+  <div class="full-page-container" :class="{ 'dark-theme': isDarkTheme }">
+    <v-btn class="theme-toggle-btn" @click="toggleTheme">
+      <v-icon>{{ isDarkTheme ? 'mdi-weather-night' : 'mdi-weather-sunny' }}</v-icon>
+    </v-btn>
     
-    <ClickHandler />
+    <div class="content">
+      <HelloWorld msg="Hello!" />
+      <FormEvent />
 
-    <v-divider></v-divider>
+      <v-divider></v-divider>
 
-    <GenerateItems />
+      <ClickHandler />
 
-    <v-divider></v-divider>
+      <v-divider></v-divider>
 
-    <Animation />
+      <GenerateItems />
 
-    <v-divider></v-divider>
+      <v-divider></v-divider>
 
-    <TimeHistory />
+      <Animation />
 
+      <v-divider></v-divider>
+
+      <TimeHistory />
+    </div>
   </div>
 </template>
 
 <script>
-
 import HelloWorld from './components/HelloWorld.vue';
 import TimeHistory from './components/TimeHistory.vue';
 import FormEvent from './components/FormEvent.vue';
@@ -40,27 +44,53 @@ export default {
     ClickHandler,
     GenerateItems,
     Animation
+  },
+  data() {
+    return {
+      isDarkTheme: false
+    };
+  },
+  methods: {
+    toggleTheme() {
+      this.isDarkTheme = !this.isDarkTheme;
+    }
   }
 }
 </script>
 
-
 <style scoped>
-header {
-  line-height: 1.5;
+.full-page-container {
+  width: 100%;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.theme-toggle-btn {
+  width: 40px;
+  height: 40px;
+  margin-left: 1.5%;
+  margin-top: 15px;
+  border-radius: 6px;
+}
+
+.dark-theme {
+  background-color: #333;
+  color: #fff;
+}
+
+.content {
+  padding: 20px;
 }
 
 @media (min-width: 1024px) {
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .content {
+    max-width: 960px;
+    margin: 0 auto;
   }
 }
 </style>
+
+
+
+
